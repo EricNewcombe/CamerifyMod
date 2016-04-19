d8080f4706004ede2b2eb178915a17f157744f0a
Eric <dukenewcombe95@gmail.com>
2016-04-19 18:06:46 -0400

    Created Files
    Main.java
    - Stores all the mod information and sets up proxies and event handlers
    
    Camera.java
    - Object to store all points and calculate position on path
    
    Chathandler.java
    - Simple handler to display messages to the user of the mod

---
+++ Minecraft/src/main/java/com/ericnewcombe/camerify/Main.java
@@ -0,0 +1,44 @@
+package com.ericnewcombe.camerify;
+
+import com.ericnewcombe.camerify.camera.gui.Camera;
+import com.ericnewcombe.camerify.proxy.CommonProxy;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Main.MODID, name = Main.MODNAME, version = Main.VERSION)
+public class Main {
+
+	public static final String MODID = "Camerify";
+	public static final String MODNAME = "Camerify Mod";
+	public static final String VERSION = "0.0.1";
+	
+	public static Camera camPath = new Camera();
+	
+	@Instance
+	public static Main instance = new Main();
+
+	@SidedProxy(clientSide="com.ericnewcombe.camerify.proxy.ClientProxy", serverSide="com.ericnewcombe.camerify.proxy.ServerProxy")
+	public static CommonProxy proxy;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent e) {
+		proxy.preInit(e);
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent e) {
+		proxy.init(e);
+	}
+
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent e) {
+		proxy.postInit(e);
+	}
+	
+}
