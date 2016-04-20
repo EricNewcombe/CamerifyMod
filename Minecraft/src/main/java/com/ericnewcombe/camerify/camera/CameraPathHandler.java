3fd8b219a7a73c6918e8f144f7f29b33282b92a8
Eric <dukenewcombe95@gmail.com>
2016-04-19 20:55:48 -0400

    Created Camera Handler
    Began to create the file which will handle the smooth transition between
    the path outlined by the camera object

---
+++ Minecraft/src/main/java/com/ericnewcombe/camerify/camera/CameraPathHandler.java
@@ -0,0 +1,53 @@
+package com.ericnewcombe.camerify.camera;
+
+import com.ericnewcombe.camerify.chat.ChatHandler;
+
+public class CameraPathHandler {
+	
+	private static Camera camera = null;
+	private static boolean moving = false;
+	private static int currentFrame = 0;
+	private static int travelTime = 200;
+	
+	public static void startTravel() {
+		
+		camera = Camera.getInstance();
+		
+		if ( camera.getSize() == 0 ) {
+			ChatHandler.sendMessage("No points to travel.");
+		}
+		if ( !moving ) {
+			ChatHandler.sendMessage("Travelling started!");
+			moving = true;
+			currentFrame = 0;
+		}
+		else {
+			ChatHandler.sendMessage("Already travelling.");
+		}
+		
+	}
+	
+	public static void endTravel() {
+		if ( moving ) {
+			ChatHandler.sendMessage("Travelling ended.");
+			moving = false;
+		}
+	}
+	
+	private static void calculatePositionInPath ( int time ) {
+		// TODO write travelling logic here
+	}
+	
+	private static float[] calculateHeadDirectionOnPath ( int time ) {
+		// TODO write the pitch, yaw transition logic here
+		return new float[2];
+	}
+	
+	private static void tick() {
+		// TODO write the logic to update camera at every tick
+	}
+	
+	public static void setTravelTime( int milliseconds ) {
+		travelTime = milliseconds;
+	}
+}
