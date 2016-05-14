a4dd55acbb71004d8497dd9de9f49e9fb68d822d
Eric <dukenewcombe95@gmail.com>
2016-05-13 21:23:30 -0400

    Added functionality and tests
    CameraPathHandler.java
    	- Began to add logic to calculate positions on splines or linear paths
    based on the size of the camera path (2 point = linear, 3 points =
    quadratic, 4 points = bezier spline)
    	- Only added linear calculations sofar as I research proper way to
    implement the other two
    	- Added functionality to convert CameraPoint to a Vec3d which Minecraft
    uses for positioning the player
    	
    CameraUnitTests.java
    	- Added testing for linear tests and converting camera points to Vec3d
    objects

--- Minecraft/src/main/java/com/ericnewcombe/camerify/camera/CameraPathHandler.java
+++ Minecraft/src/main/java/com/ericnewcombe/camerify/camera/CameraPathHandler.java
@@ -1,19 +1,36 @@
 package com.ericnewcombe.camerify.camera;
 
+import java.util.ArrayList;
+
 import com.ericnewcombe.camerify.chat.ChatHandler;
+
+import net.minecraft.util.math.Vec3d;
 
 public class CameraPathHandler {
 	
-	private static Camera camera = null;
+	/** Tracks whether the player is in motion through the points */
 	private static boolean moving = false;
+	
+	/**  The current frame that is being displayed in the path */
 	private static int currentFrame = 0;
+	
+	/** The time in milliseconds to travel the path */
 	private static int travelTime = 200;
+	
+	/** List of all {@link CameraPoint}s contained in the path */
+	private static ArrayList<CameraPoint> path;
+	
+	/**
+	 * Starts the travelling of the {@link CameraPoint}. If there are points to traverse then it displays a message that it has started to the user, then removes control
+	 * and begins to traverse points. If there is no points to traverse, or it it already travelling then it displays an appropriate message
+	 * @return Whether the camera successfullly started travelling or not
+	 */
 	
 	public static boolean startTravel() {
 		
-		camera = Camera.getInstance();
+		updatePoints();
 		
-		if ( camera.getSize() == 0 ) {
+		if ( Camera.getSize() == 0 ) {
 			ChatHandler.sendMessage("No points to travel.");
 			return false;
 		}
@@ -30,6 +47,7 @@ public class CameraPathHandler {
 		
 	}
 	
+
 	public static boolean endTravel() {
 		if ( moving ) {
 			ChatHandler.sendMessage("Travelling ended.");
@@ -39,20 +57,140 @@ public class CameraPathHandler {
 		return false; // Not moving
 	}
 	
-	private static void calculatePositionInPath ( int time ) {
-		// TODO write travelling logic here
+	private static Vec3d calculatePositionInPath ( double pointPositionInPath) {
+		
+		if ( Camera.getSize() == 1 ) { return generateVecFromPoint (path.get(0)); }
+		if ( Camera.getSize() == 2 ) { return calculateLinearPosition( pointPositionInPath ); }
+		if ( Camera.getSize() == 3 ) { return calculateQuadraticPosition ( pointPositionInPath ); }
+		else { return calculateSplinePosition ( pointPositionInPath ); }
+		
+	}
+
+	/**
+	 * Creates a {@link Vec3d} representation of a {@link CameraPoint}
+	 * @param p {@link CameraPoint} to be represented
+	 * @return {@link Vec3d} representation of the {@link CameraPoint}
+	 */
+	public static Vec3d generateVecFromPoint(CameraPoint p) {
+		return new Vec3d(p.x, p.y, p.z);
 	}
 	
-	private static float[] calculateHeadDirectionOnPath ( int time ) {
+	public static Vec3d calculateSplinePosition(double pointPositionInPath) {
+		
+		Vec3d pos;
+		
+		pointPositionInPath = Math.ceil( pointPositionInPath * 1000 ) / 1000;
+		ArrayList <CameraPoint> path = Camera.getPath();
+		
+		// For debugging purposes
+		ChatHandler.sendMessage(String.valueOf(pointPositionInPath));
+		
+		double x = 0,
+			   y = 0,
+			   z = 0;
+		
+		int startPoint = (int) Math.floor(pointPositionInPath);
+		
+		
+		if ( startPoint >= Camera.getSize() - 4 ) { startPoint = Camera.getSize() - 4; }
+		
+		// TODO write travelling logic here
+		
+		
+		pos = new Vec3d(x, y, z);
+		
+		return pos;
+	}
+
+	public static Vec3d calculateQuadraticPosition(double pointPositionInPath) {
+		Vec3d pos;
+		
+		pointPositionInPath = Math.ceil( pointPositionInPath * 1000 ) / 1000;
+		ArrayList <CameraPoint> path = Camera.getPath();
+		
+		// For debugging purposes
+		ChatHandler.sendMessage(String.valueOf(pointPositionInPath));
+		
+		double x = 0,
+			   y = 0,
+			   z = 0;
+		
+		int startPoint = (int) Math.floor(pointPositionInPath);
+		
+		
+		if ( startPoint >= Camera.getSize() - 4 ) { startPoint = Camera.getSize() - 4; }
+		
+		// TODO write travelling logic here
+		
+		
+		pos = new Vec3d(x, y, z);
+		
+		return pos;
+	}
+
+	/**
+	 * Calculates the position between two points in a linear way
+	 * @param pointPositionInPath double between 0 and 1 representing the percentage completed between two points
+	 * @return {@link Vec3d} point of where the player should be at the particular point in the path
+	 */
+	public static Vec3d calculateLinearPosition(double pointPositionInPath) {
+		Vec3d pos;
+		
+		pointPositionInPath = Math.ceil( pointPositionInPath * 1000 ) / 1000;
+		ArrayList <CameraPoint> path = Camera.getPath();
+		
+		// For debugging purposes
+		ChatHandler.sendMessage(String.valueOf(pointPositionInPath));
+		
+		double x = 0,
+			   y = 0,
+			   z = 0;
+		
+		int startPoint = (int) Math.floor(pointPositionInPath);
+		
+		// TODO write travelling logic here
+		
+		// If it is at the beginning or the end of the path just return that point, else set the values as a combination of the two points
+		// based on the amount between the two points has been travelled
+		
+		x = path.get(0).x * ( 1 - pointPositionInPath ) + path.get(1).x * pointPositionInPath;
+		y = path.get(0).y * ( 1 - pointPositionInPath ) + path.get(1).y * pointPositionInPath;
+		z = path.get(0).z * ( 1 - pointPositionInPath ) + path.get(1).z * pointPositionInPath;
+		
+		pos = new Vec3d(x, y, z);
+		
+		return pos;
+	}
+
+	private static float[] calculateHeadDirectionOnPath ( double pointPositioninPath ) {
 		// TODO write the pitch, yaw transition logic here
 		return new float[2];
 	}
 	
+	/**
+	 * Updates the player position and view angles based on the the amount of the path that has been travelled
+	 * then updates the values increasing the amount of time passed
+	 */
 	public static void tick() {
 		// TODO write the logic to update camera at every tick
+		if ( moving ) {
+			if ( Camera.getSize() > 1 ) {
+				
+			}
+		}
 	}
 	
+	/**
+	 * sets the time that it will take to traverse the path in milliseconds
+	 * @param milliseconds Number of milliseconds that it will take to traverse the path
+	 */
 	public static void setTravelTime( int milliseconds ) {
 		travelTime = milliseconds;
+	}
+	
+	/** updates the Camera path to the most recent points */
+
+	private static void updatePoints() {
+		path = Camera.getPath();
 	}
 }
--- Minecraft/src/main/java/com/ericnewcombe/tests/CameraUnitTests.java
+++ Minecraft/src/main/java/com/ericnewcombe/tests/CameraUnitTests.java
@@ -7,7 +7,10 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 import com.ericnewcombe.camerify.camera.Camera;
+import com.ericnewcombe.camerify.camera.CameraPathHandler;
 import com.ericnewcombe.camerify.camera.CameraPoint;
+
+import net.minecraft.util.math.Vec3d;
 
 public class CameraUnitTests {
 	
@@ -61,6 +64,39 @@ public class CameraUnitTests {
 
 	@Test
 	public void testCameraPathHandler() {
+		
+		// initialize camera points
+		CameraPoint a = new CameraPoint(50, 50, 50, 10, 10),
+					b = new CameraPoint(100, 45, 60, 60, 80),
+					c = new CameraPoint(110, 55, 80, 30, 60),
+					d = new CameraPoint(160, 435, 10, 50, 70);
+		
+		Camera.clearPath();
+		
+		// add to camera
+		Camera.addPoint(a);
+		Camera.addPoint(b);
+		Camera.addPoint(c);
+		Camera.addPoint(d);
+		
+		// Test converting point to Vec3D
+		
+		Vec3d vecA = CameraPathHandler.generateVecFromPoint(a);
+		assertEquals(vecA.xCoord, a.x, 0);
+		assertEquals(vecA.yCoord, a.y, 0);
+		assertEquals(vecA.zCoord, a.z, 0);
+		
+		
+		
+		// Linear Path
+		
+		assertEquals(CameraPathHandler.calculateLinearPosition(0).xCoord, a.x, 0);
+		assertEquals(CameraPathHandler.calculateLinearPosition(1).xCoord, b.x, 0);
+		assertEquals(CameraPathHandler.calculateLinearPosition(0.5).xCoord, 75, 0);
+		assertEquals(CameraPathHandler.calculateLinearPosition(0.75).xCoord, 87.5, 0);
+		assertEquals(CameraPathHandler.calculateLinearPosition(0.25).xCoord, 62.5, 0);
+		
+		
 		
 	}
 }
